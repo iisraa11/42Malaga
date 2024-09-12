a9038b39a3cbdfcc2af5df061779d1fb276ef973
iisraa11 <112698427+iisraa11@users.noreply.github.com>
2024-09-12 19:35:01 +0200

    Add files via upload

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isrguerr <isrguerr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 17:04:20 by isrguerr          #+#    #+#             */
+/*   Updated: 2024/09/12 19:18:15 by isrguerr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char *s;
+    unsigned char *d;
+    size_t i;
+
+    s = (char *)src;
+    d = (char *)dest;
+    i = 0;
+    while (i < n)
+    {
+        d[i] = s[i];
+        i++;
+    }
+    return (dest);
+}
