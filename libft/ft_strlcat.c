5a14f9ef1f710e1c02c723c988b3fa3e5d34c729
IsraelGuerrero <israelguerreroortiz83@gmail.com>
2024-09-16 10:50:38 +0200

    fix strlcpy

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,13 +6,25 @@
 /*   By: isrguerr <isrguerr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 19:53:41 by isrguerr          #+#    #+#             */
-/*   Updated: 2024/09/12 19:58:30 by isrguerr         ###   ########.fr       */
+/*   Updated: 2024/09/14 10:16:47 by isrguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t strlcat(char *dst, const char *src, size_t size)
+size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
+    size_t i;
+    size_t j;
+
+    i = 0;
+    while (dst[i] != '\0')
+        i++;
+    while (j < size - 1 && src[i] != '\0')
+    {
+        dst[i] = src[j];
+        j++;
+    }
     
+
 }
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,29 +6,37 @@
 /*   By: isrguerr <isrguerr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 19:52:04 by isrguerr          #+#    #+#             */
-/*   Updated: 2024/09/13 19:53:08 by isrguerr         ###   ########.fr       */
+/*   Updated: 2024/09/16 10:50:04 by isrguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-    size_t i;
-    size_t j;
+	size_t	i;
+	size_t	j;
 
-    i = 0;
-    while (src[i] != '\0')
-        i++;
-    if (size < 0)
-    {
-        j = 0;
-        while(j < size - 1 && src[i] != '\0')
-        {
-            dst[j] = src[j];
-            j++;
-        }
-        dst[j] = '\0';
-    }
-    return (i);
+	i = 0;
+	j = 0;
+	while (src[i] != '\0')
+		i++;
+	if (i < size)
+	{
+		while (src[j] != '\0')
+		{
+			dst[j] = src[j];
+			j++;
+		}
+	}
+	else
+	{
+		while (j < size - 1)
+		{
+			dst[j] = src[j];
+			j++;
+		}
+		dst[j] = '\0';
+	}
+	return (i);
 }
