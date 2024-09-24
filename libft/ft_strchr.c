c7b9e86150c783f9b2b939383ac2a09a14c9f734
IsraelGuerrero <israelguerreroortiz83@gmail.com>
2024-09-24 12:44:15 +0200

    commit changes

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: isrguerr <isrguerr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 15:30:01 by isrguerr          #+#    #+#             */
-/*   Updated: 2024/09/16 19:39:13 by isrguerr         ###   ########.fr       */
+/*   Updated: 2024/09/24 11:54:29 by isrguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,11 @@
 char *ft_strchr(const char *s, int c)
 {
     unsigned int i;
-    char *chr;
     i = 0;
     while (s[i])
     {
         if (s[i] == c)
-            return (chr = (unsigned char *)&s[i]);
+            return ((unsigned char *)&s[i]);
         i++;
     }
     return (NULL);
--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: isrguerr <isrguerr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 20:03:41 by isrguerr          #+#    #+#             */
-/*   Updated: 2024/09/17 12:01:31 by isrguerr         ###   ########.fr       */
+/*   Updated: 2024/09/17 15:41:10 by isrguerr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 char *ft_strchr(const char *s, int c)
 {
     unsigned int i;
-    char *chr;
 
     i = ft_strlen(s) - 1;
-    while (s[i])
+    while (s[i] != '\0')
     {
 
         if (s[i] == c)
-            return (chr = (unsigned char *)&s[i]);
+            return ((unsigned char *)&s[i]);
         i--;
     }
     return (NULL);
