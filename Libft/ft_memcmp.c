8e58080791e0243cb021ed253ec50382cdef7903
iisraa11 <112698427+iisraa11@users.noreply.github.com>
2024-09-30 19:28:37 +0200

    Add files via upload

---
+++ Libft/ft_memcmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isrguerr <isrguerr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 13:03:32 by isrguerr          #+#    #+#             */
+/*   Updated: 2024/09/25 18:42:48 by isrguerr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t i;
+	unsigned char *str1;
+	unsigned char *str2;
+	size_t counter;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			counter = counter + (str1[i] - str2[i]);
+			i++;
+		}
+		return (counter);
+	}
+	return (0);
+}
---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isrguerr <isrguerr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 19:53:41 by isrguerr          #+#    #+#             */
+/*   Updated: 2024/09/24 17:40:13 by isrguerr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t srclen;
+	size_t dstlen;
+	size_t i;
+
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	i = 0;
+	if (size > 0)
+	{
+		while ((dstlen + i) < size - 1 && src[i] != '\0')
+		{
+			dst[dstlen + i] = src[i];
+			i++;
+		}
+		dst[dstlen + i] = '\0';
+	}
+	return (srclen + dstlen);
+}
